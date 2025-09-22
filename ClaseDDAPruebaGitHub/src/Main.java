9ff9c476db96504a9b845a71b60e7d3417b54a04
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2025-09-22 20:13:39 -0300

    Abr

--- ClaseDDAPruebaGitHub/src/Main.java
+++ ClaseDDAPruebaGitHub/src/Main.java
@@ -4,7 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Persona persona  = new Persona(100, "Nahuel");
+        Persona persona2  = new Persona(100, "Paola");
+        Persona persona3  = new Persona(100, "Alejo");
 
         System.out.println(persona.toString());
+        System.out.println(persona2.toString());
+        System.out.println(persona3.toString());
+
+
     }
 }
