ab0ab3f8ebfa3bb18ed354a4e24778d58942516d
Nahuel Pages Oviedo <nahuel01pages@gmail.com>
2025-10-08 21:36:33 -0300

    Menu Main

--- ClaseDDAPruebaGitHub/src/Main.java
+++ ClaseDDAPruebaGitHub/src/Main.java
@@ -5,11 +5,39 @@ import PaquetePersonas.Guerrero;
 import PaquetePersonas.Persona;
 import PaquetePersonas.Mago;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     int opcion = 0;
 
-    
+        System.out.println("Menu de opciones");
+
+        while (opcion != -1){
+
+            System.out.println("Opcion 1: Prueba de Natacion");
+            System.out.println("Opcion 2: Preuba de Auto");
+
+            opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Paola");
+                    break;
+                case 2:
+                    System.out.println("Sophie");
+                    break;
+                case -1:
+                    System.out.println("Saliendo");
+                    break;
+                default:
+                    System.out.println("Mal ahi che");
+                    break;
+            }
+
+        }
 
     }
 }
---
+++ ClaseDDAPruebaGitHub/src/abr/atlPrueba.java
@@ -0,0 +1,4 @@
+package abr;
+
+public class atlPrueba {
+}
